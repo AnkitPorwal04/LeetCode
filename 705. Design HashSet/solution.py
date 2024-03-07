e2341eb735ad2210182a01a1b5e68fd8caf1b5d2
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-07 17:07:52 +0530

    Create solution.py

---
+++ 705. Design HashSet/solution.py
@@ -0,0 +1,20 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.hashset = set()
+
+    def add(self, key: int) -> None:
+        self.hashset.add(key)
+
+    def remove(self, key: int) -> None:
+        self.hashset.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return True if key in self.hashset else False
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)

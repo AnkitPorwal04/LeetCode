92246d3ef73b521782cad5bb0be416d482c8b00a
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-08 15:17:53 +0530

    Update Solution.py

--- 9. Palindrome Number/Solution.py
+++ 9. Palindrome Number/Solution.py
@@ -1,6 +1,11 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if str(x) == str(x)[::-1]:
+        rev_x = 0
+        x_temp = x
+        while x_temp>0:
+            rev_x = rev_x*10 + (x_temp%10)
+            x_temp //= 10
+        if x == rev_x:
             return True
         else:
             return False

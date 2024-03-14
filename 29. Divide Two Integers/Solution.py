269a32e466011b22febae6bcaf60678560001da9
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-14 19:43:33 +0530

    Create Solution.py

---
+++ 29. Divide Two Integers/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        sign = -1 if (dividend >= 0 and divisor < 0) or (dividend < 0 and divisor >= 0) else 1
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+        result = len(range(0, dividend-divisor+1, divisor))
+        if sign == -1:
+            result = -result
+        minus_limit = -(2**31)
+        plus_limit = (2**31 - 1)
+        result = min(max(result, minus_limit), plus_limit)
+        return result

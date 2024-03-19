74b66289651c4883394d87b3cf43a4501b2e0848
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-19 16:30:10 +0530

    Create Solution.py

---
+++ 88. Merge Sorted Array/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(n):
+            nums1.remove(0)
+            nums1.append(nums2[i])
+        nums1.sort()
+        return nums1

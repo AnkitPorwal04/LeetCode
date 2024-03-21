cb59330cbb524ab310b51e1f81085afeef850d69
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-21 14:24:10 +0530

    Create Solution.py

---
+++ 206. Reverse Linked List/Solution.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previous, current = None, head
+        while current != None:
+            temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
+        return previous

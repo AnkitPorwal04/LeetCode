ee8d14239a6e7516f7cdc3d7d708078498abd1e9
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-25 11:11:32 +0530

    Create Solution.py

---
+++ 1389. Create Target Array in the Given Order/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums)):
+            ans.insert(index[i], nums[i])
+        return ans

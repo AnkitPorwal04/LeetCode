805c83740592ed37cf1d01de6f2005a024243ccc
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-28 13:09:11 +0530

    Create Solution.py

---
+++ 605. Can Place Flowers/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed.insert(0, 0)
+        flowerbed.append(0)
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
+                flowerbed[i] = i
+                if n: n -= 1
+        if n:
+            return False
+        else:
+            return True

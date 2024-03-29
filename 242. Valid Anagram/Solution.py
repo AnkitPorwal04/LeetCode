b334aea1450bd2ebfb11699ea6aa43c274a0daa8
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-29 13:29:59 +0530

    Update Solution.py

--- 242. Valid Anagram/Solution.py
+++ 242. Valid Anagram/Solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s = list(s)
-        t = list(t)
-        s.sort()
-        t.sort()
+        s = sorted(s)
+        t = sorted(t)
         if s == t:
             return True
         else:

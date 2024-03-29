109031f1c74401444f3f4a002d176b2329cf58dc
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-03-29 13:28:31 +0530

    Create Solution.py

---
+++ 242. Valid Anagram/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        if s == t:
+            return True
+        else:
+            return False

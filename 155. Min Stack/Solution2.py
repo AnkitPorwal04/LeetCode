44a17b7167d22a0ecef1d9bb42d3d9cc262a723f
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-04-06 09:56:52 +0530

    Create Solution2.py

---
+++ 155. Min Stack/Solution2.py
@@ -0,0 +1,29 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            val = min(val, self.minStack[-1])
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

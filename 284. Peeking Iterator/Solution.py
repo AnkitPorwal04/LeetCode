500b65c450a5b41002da85df04b99547f49ac643
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-04-10 10:41:46 +0530

    Update Solution.py

--- 284. Peeking Iterator/Solution.py
+++ 284. Peeking Iterator/Solution.py
@@ -1,14 +1,59 @@
-class Solution:
-    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        deck.sort()
+# Below is the interface for Iterator, which is already defined for you.
+#
+# class Iterator:
+#     def __init__(self, nums):
+#         """
+#         Initializes an iterator object to the beginning of a list.
+#         :type nums: List[int]
+#         """
+#
+#     def hasNext(self):
+#         """
+#         Returns true if the iteration has more elements.
+#         :rtype: bool
+#         """
+#
+#     def next(self):
+#         """
+#         Returns the next element in the iteration.
+#         :rtype: int
+#         """
+
+class PeekingIterator:
+    def __init__(self, iterator):
+        """
+        Initialize your data structure here.
+        :type iterator: Iterator
+        """
+        self.iterator = iterator
+        self.current = self.iterator.next() if self.iterator.hasNext() else None
         
-        n = len(deck)
-        result = [0] * n
-        indices = deque(range(n))
+
+    def peek(self):
+        """
+        Returns the next element in the iteration without advancing the iterator.
+        :rtype: int
+        """
+        return self.current
         
-        for card in deck:
-            idx = indices.popleft()  
-            result[idx] = card      
-            if indices:             
-                indices.append(indices.popleft())     
-        return result
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        value = self.current
+        self.current = self.iterator.next() if self.iterator.hasNext() else None
+        return value
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self.current != None
+        
+
+# Your PeekingIterator object will be instantiated and called as such:
+# iter = PeekingIterator(Iterator(nums))
+# while iter.hasNext():
+#     val = iter.peek()   # Get the next element but not advance the iterator.
+#     iter.next()         # Should return the same value as [val].

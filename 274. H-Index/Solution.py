689780d0f5b0241ff313de913f838b24f6343caa
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-04-17 18:51:43 +0530

    Create Solution.py

---
+++ 274. H-Index/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        citations.sort()
+
+        for i,v in enumerate(citations):
+            if n - i <= v:
+                return n - i
+        return 0

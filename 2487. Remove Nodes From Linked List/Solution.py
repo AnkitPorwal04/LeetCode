14f0cf08449fd9c2b7b73b83e54f637df4731703
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-05-06 09:06:50 +0530

    Create Solution.py

---
+++ 2487. Remove Nodes From Linked List/Solution.py
@@ -0,0 +1,21 @@
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        stack = []
+        while cur:
+            while stack and stack[-1].val < cur.val:
+                a = stack.pop()
+            stack.append(cur)
+            cur = cur.next
+        
+        nxt = None
+        while stack:
+            cur = stack.pop()
+            cur.next = nxt
+            nxt = cur
+        
+        return cur

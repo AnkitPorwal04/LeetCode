13ccaa016f3b52722f2a778b913098efc304d6c9
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-05-08 09:41:04 +0530

    Create Better Solution.py

---
+++ 506. Relative Ranks/Better Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        sorted_score = sorted(score, reverse=True)
+        medals = ["Gold Medal", "Silver Medal", "Bronze Medal"]
+        rank_mapping = {score: medals[i] if i < 3 else str(i + 1) for i, score in enumerate(sorted_score)}
+        return [rank_mapping[score] for score in score]

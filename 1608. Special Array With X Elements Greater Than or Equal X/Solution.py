a526f08e034df392ec180d5f9b9021287368ab30
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-05-27 10:16:55 +0530

    Update Solution.py

--- 1608. Special Array With X Elements Greater Than or Equal X/Solution.py
+++ 1608. Special Array With X Elements Greater Than or Equal X/Solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         n = len(nums)
-        for i in range(0, n+1):
+        for i in range(n, -1, -1):
             c = 0
             for j in nums:
                 if j >= i:

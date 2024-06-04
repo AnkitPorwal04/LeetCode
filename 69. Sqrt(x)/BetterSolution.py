53db62fb1261ebf4a3baaecff841693511a0a4fb
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-06-04 15:18:48 +0530

    Create BetterSolution.py

---
+++ 69. Sqrt(x)/BetterSolution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        first, last = 1, x
+        while first <= last:
+            mid = first + (last - first) // 2
+            if mid == x // mid:
+                return mid
+            elif mid > x // mid:
+                last = mid - 1
+            else:
+                first = mid + 1
+        return last

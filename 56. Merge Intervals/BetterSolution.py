9e8dc78ef59e617af280364f2d979b1ed8fe518b
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-06-04 14:41:54 +0530

    Create BetterSolution.py

---
+++ 56. Merge Intervals/BetterSolution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals = sorted(intervals, key=lambda x: x [0])
+
+        ans = []
+
+        for interval in intervals:
+            if not ans or ans[-1][1] < interval[0]:
+                ans.append(interval)
+            else:
+                ans[-1][1] = max(ans[-1][1], interval[1])
+        
+        return ans

e672ad3063b86c1fa183421715be93577884e0f2
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-06-06 10:36:30 +0530

    Create Solution.py

---
+++ 1296. Divide Array in Sets of K Consecutive Numbers/Solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+        nums.sort()
+
+        dq = collections.deque([])
+
+        #print(card_ct)
+        for card in nums:
+            #print(card)
+            if len(dq) >0:
+                if dq[-1][1] == card -1:
+                    item = dq.pop()
+                    item[0] += 1
+                    item[1] = card
+                elif dq[-1][1] == card:
+                    item = [1,card]
+                else:
+                    return False # in this case there will not be anything to fill the open straight
+            else:
+                item = [1,card]
+        
+            if item[0] < k:
+                dq.appendleft(item) # move it to left so we can deal with new new ones 
+
+        
+            
+                    
+
+        return len(dq) == 0

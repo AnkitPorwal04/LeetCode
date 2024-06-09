41508c31cbfb85d4dbd7d4cbd1b42603515af7f3
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-06-09 13:43:55 +0530

    Create Solution.py

---
+++ 974. Subarray Sums Divisible by K/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        count = 0
+        prefix_sum = 0
+        prefix_map = {0: 1}  
+        
+        for num in nums:
+            prefix_sum += num
+            mod = prefix_sum % k
+            if mod < 0:  
+                mod += k
+            if mod in prefix_map:
+                count += prefix_map[mod]
+                prefix_map[mod] += 1
+            else:
+                prefix_map[mod] = 1
+        
+        return count

71badeab9175c8fbb5608e1ff4ecca26120e1473
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2024-07-02 13:15:08 +0530

    Create BetterSolution.py

---
+++ 350. Intersection of Two Arrays II/BetterSolution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        nums1.sort()
+        nums2.sort()
+
+        ans = []
+
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                ans.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return ans

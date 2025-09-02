f6f1e6cd14c0f476833abcdc9df308512664ea8c
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2025-09-02 21:53:00 +0530

    Create Solution for 3025 in python.

---
+++ 3025. Find the Number of Ways to Place People I/Solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numberOfPairs(self, points: list[list[int]]) -> int:
+        n = len(points)
+
+        # Sort by x ascending, and if x is the same, y descending
+        points.sort(key=lambda p: (p[0], -p[1]))
+
+        result = 0
+
+        for i in range(n):
+            x1, y1 = points[i]a
+            bestY = float("-inf")
+
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+
+                # Must be "lower-right": y2 ≤ y1
+                if y2 > y1:
+                    continue
+
+                if y2 > bestY:
+                    result += 1
+                    bestY = y2
+
+        return result

4bdd16970307211cbce5423a2b74fcff059bb148
Ankit Porwal <96345105+AnkitPorwal04@users.noreply.github.com>
2025-12-28 23:09:29 +0530

    Create Solution(better).py

---
+++ 1351. Count Negative Numbers in a Sorted Matrix/Solution(better).py
@@ -0,0 +1,17 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        i = m - 1
+        j = 0
+        res = 0
+
+        while i >= 0 and j < n:
+            if grid[i][j] < 0:
+                res += n - j
+                i -= 1
+            else:
+                j += 1
+
+        return res
